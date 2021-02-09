8d7d9f57fd578d919855ceab8fa712940411892a
Ian MacDonald <ian.macdonald@ianmacdonald.scot>
2021-02-09 19:49:46 +0000

    Initial implementation of client login using OAuth2

---
+++ src/main/java/scot/ianmacdonald/cakemgr/restclient/security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package scot.ianmacdonald.cakemgr.restclient.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and().oauth2Login().and().logout()
+				.invalidateHttpSession(true).clearAuthentication(true).deleteCookies("JSESSIONID").permitAll();
+	}
+}
