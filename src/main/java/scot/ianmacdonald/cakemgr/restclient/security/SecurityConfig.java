bab2ac3eb50cff373e5e38f41b00f55995c0c776
Ian MacDonald <ian.macdonald@ianmacdonald.scot>
2021-02-11 18:42:11 +0000

    Improve error display and add OAuth2 logout button

--- src/main/java/scot/ianmacdonald/cakemgr/restclient/security/SecurityConfig.java
+++ src/main/java/scot/ianmacdonald/cakemgr/restclient/security/SecurityConfig.java
@@ -1,16 +1,21 @@
 package scot.ianmacdonald.cakemgr.restclient.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	ClientRegistrationRepository clientRegistrationRepository;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated().and().oauth2Login().and().logout()
-				.invalidateHttpSession(true).clearAuthentication(true).deleteCookies("JSESSIONID").permitAll();
+		.invalidateHttpSession(true).clearAuthentication(true).deleteCookies("JSESSIONID").permitAll();
 	}
+
 }
